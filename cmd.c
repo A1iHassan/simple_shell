ab808e88563f3a6de254511e975ec19783d4d5f8
ALI <ali012wkout@gmail.com>
2023-11-15 02:34:59 +0200

    -

---
+++ cmd.c
@@ -0,0 +1,57 @@
+#include "shell.h"
+
+/**
+ * execmd - a caller function for execve
+ * @argv: array of command and its argument
+ */
+void execmd(char **argv)
+{
+	char *command = argv[0], *cmd = execute(command);
+	extern char **environ;
+	pid_t process;
+	int pr;
+
+	if (cmd)
+	{
+		process = fork();
+		if (process == 0)
+		{
+			if (argv)
+			{
+				if (execve(cmd, argv, environ) == -1)
+				{
+					perror("execve");
+					write(2, "./hsh: 1: ", 10);
+					write(2, command, strlen(command));
+					write(2, ": not found\n", 12);
+					// free(cmd);
+					// _free(argv, _strlen_(argv));
+					// exit(127);
+				}
+				// free(cmd);
+			}
+		}
+		else if (process < 0)
+		{
+			// free(cmd);
+			// _free(argv, _strlen_(argv));
+			perror("FORK FAILED");
+			// exit(EXIT_FAILURE);
+		}
+		// free(cmd);
+		// _free(argv, _strlen_(argv));
+		wait(&pr);
+	}
+	else
+	{
+		write(2, "./hsh: 1: ", 10);
+		write(2, command, strlen(command));
+		write(2, ": not found\n", 12);
+		// free(cmd);
+		// _free(argv, _strlen_(argv));
+		// exit(127);
+	}
+
+
+
+}
