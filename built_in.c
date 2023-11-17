b11ea22ecf075f31616e527630992af94de6073e
dina3538 <dinaaltayeb1234@gmail.com>
2023-11-17 04:06:01 -0800

    a

--- built_in.c
+++ built_in.c
@@ -2,6 +2,7 @@
 #include<unistd.h>
 #include <sys/stat.h>
 
+int  comp_built(char *line);
 /**
  * built_in - checks for built in commands to be executed
  * @line: give input string
@@ -11,55 +12,64 @@
 int built_in(char *line)
 {
 	char **a = NULL;
-	int i = 0;
 	int count = 0;
-	char *previous_dir = getenv("OLDPWD");
-	char *dir = getenv("HOME");
-	char *pwd = getenv("PWD");
-	char *text = "./hsh: 1: cd: can't cd to ";
 	int exit_status;
+
 	a = tokenize(line);
 	count = _strlen_(a);
 
-	(void)previous_dir,(void)i; (void)pwd;
 	if (_strcmp(a[0], "exit") == 0)
 	{
-		if (count == 1){
+		if (count == 1)
+		{
 			free(line);
 			_free(a, _strlen_(a));
-			exit(0);
-		}
-		else if (count == 2) {
+			exit(0); }
+		else if (count == 2)
+		{
 			exit_status = atoi(a[1]);
 			if (exit_status < 0 || (exit_status == 0 && a[1][0] != '0'))
-			{ 
+			{
 				write(STDERR_FILENO, "./hsh: 1: exit: ", 16);
 				write(STDERR_FILENO, "Illegal number: ", 16);
 				write(STDERR_FILENO, a[1], _strlen(a[1]));
 				write(2, "\n", 1);
 				free(line);
 				_free(a, _strlen_(a));
-				exit(2); } 
-			else 
-			{ 
+				exit(2); }
+			else
+			{
 				free(line);
 				_free(a, _strlen_(a));
 				exit(exit_status); }
-
 		}
-		else {
+		else
+		{
 			_free(a, _strlen_(a));
 
-
-			return (1); 
-		}
+		return (1); }
 	}
-	else if (_strcmp(a[0], "env") == 0)
+	else
 	{
-		_envp_();
-		_free(a, _strlen_(a));
-		return (1);
-	} else if (_strcmp(a[0], "cd") == 0)
+		_free(a, count); }
+	return (0);
+}
+/**
+ * comp_built - check if command built in
+ * @line: the line
+ * Return: 0 or 1
+ */
+int  comp_built(char *line)
+{
+	char **a = NULL;
+	int count = 0;
+	char *dir = getenv("HOME");
+	char *text = "./hsh: 1: cd: can't cd to ";
+
+	a = tokenize(line);
+	count = _strlen_(a);
+
+	if (_strcmp(a[0], "cd") == 0)
 	{
 		if (count == 1)
 		{
@@ -69,45 +79,59 @@ int built_in(char *line)
 			return (1); }
 		else if (count == 2 && _strcmp(a[1], "-") == 0)
 		{
-			chdir(dir);
-			_cd();
+			cd_old(dir);
 			_free(a, _strlen_(a));
 			return (1);
 		} else if (count == 2)
+		{
 			if (chdir(a[1]) == 0)
 			{
 				_cd();
 				_free(a, _strlen_(a));
 				return (1);
-			} else
-			{
-				write(2, text, _strlen(text));
-				write(2, a[1], _strlen(a[1]));
-				write(2, "\n", 1);
-				_free(a, _strlen_(a)); 
-				return (1); }
-			else 
+			}
+			else
 			{
 				write(2, text, _strlen(text));
 				write(2, a[1], _strlen(a[1]));
 				write(2, "\n", 1);
 				_free(a, _strlen_(a));
-				return (1); } }
+				return (1); } } }
+	else
+	{
+		_free(a, count); }
+	return (0); }
+/**
+* more_built - check if command is built in
+* @line: the line
+* Return: 0 or 1
+*/
+int more_built(char *line)
+{
+	char **a = NULL;
+	int count = 0;
+
+	a = tokenize(line);
+	count = _strlen_(a);
+
+	if (_strcmp(a[0], "unsetenv") == 0)
+	{
+		handel_unset(a);
+		_free(a, _strlen_(a));
+		return (1); }
 	else if (_strcmp(a[0], "setenv") == 0)
 	{
 		handle_set(a);
 		_free(a, count);
 		return (1); }
-	else if (_strcmp(a[0], "unsetenv") == 0)
+	else if (_strcmp(a[0], "env") == 0)
 	{
-		handel_unset(a);
-		_free(a, _strlen_(a));	
-		return (1); }
+		_envp_();
+		_free(a, _strlen_(a));
+	}
 	else
-	{	execmd(a);
+	{       execmd(a);
 		_free(a, count);
 	}
-
-	
 	return (0);
 }
