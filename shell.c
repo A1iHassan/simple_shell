8e9fae94c141d9505c260fecae3bc97b5354e1dc
ALI <ali012wkout@gmail.com>
2023-11-17 20:56:05 +0200

    -

--- shell.c
+++ shell.c
@@ -13,46 +13,39 @@ int main(int argc, char **argv, char **envp)
 	char *line = NULL;
 	size_t n = 0;
 	ssize_t charac;
-/*	int i = 0;*/
+	int i = 0;
 
 	(void)argc; (void)argv;
 	while (1)
 	{
 		if (isatty(0))
-		{
-			_print("$ "); }
+			_print("$ ");
 		charac = getline(&line, &n, stdin);
 		if (charac == -1)
 		{
 			if (isatty(0))
-			{
-				_print("\n"); }
+				_print("\n");
 			break; }
 		if (count(line) == 0)
 			continue;
-		line[_strlen(line) - 1] = '\0';
-	/**	temp = malloc(sizeof(char) * strlen(line));
-		while (line[i])
+		while (1)
 		{
-			if (line[i] != '#' && line[i] != '"')
-				temp[i] = line[i];
-			else
+			if (line[i] == '#' && (line[i - 1] == '"' || line[i - 1] == ' '))
 				{
-					temp[i] = '\0';
+					line[i] = '\0';
 					break; }
-			i++; }*/
+			else if (line[i] == '\n')
+			{
+				line[i] = '\0';
+				break; }
+			i++; }
 		if (_strlen(line) == 0)
-		{
 			continue;
-		}
 		if (built_in(line) == 1)
 			continue;
 		if (comp_built(line) == 1)
 			continue;
 		if (more_built(line, envp) == 1)
-			continue; 
-
-}
-/*	free(temp);*/
+			continue; }
 	free(line);
 	return (0); }
